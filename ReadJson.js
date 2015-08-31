82e541b4ac8f4e80420bd6354e9957c40c84043d
Bill <b20103014578@gmail.com>
2015-08-31 22:40:52 +0800

    save2

--- ReadJson.js
+++ ReadJson.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.readFile('./example.json','utf8',function (err,data) {
+fs.readFile('./save.json','utf8',function (err,data) {
   // body...
   if(err)
     console.log("read fail");
