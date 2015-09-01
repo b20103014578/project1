d219844617810cc2c719eae372a120057645d529
Bill <b20103014578@gmail.com>
2015-09-01 22:02:20 +0800

    xlsx

---
+++ createxlsx.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var officegen = require('officegen');
+
+// create xlsx
+var xlsx = officegen('xlsx');
+xlsx.name = 'My excel';
+
+//create new sheet
+var sheet = xlsx.makeNewSheet();
+sheet data[10]=[];
+sheet.data[10][2] = 'Hello';
+sheet.setCell('G8','Node.js');
+
+//save as a new file
+var output = fs.createWriteStream('output.xlsx');
+xlsx.generate(output);
